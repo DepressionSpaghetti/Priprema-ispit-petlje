46ad0c3e2af8967b8339b54aaf996a096183d920
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-01-22 20:39:11 +0100

    Add files via upload

---
+++ Priprema_ispit_pet2/Priprema_ispit_pet2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_ispit_pet2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = ' ';
+            int x, max = 0;
+            Console.WriteLine("Unesite riječ:");
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                x = 0;
+                for (int j = 0; j < s.Length; j++)
+                    if (s[i] == s[j])
+                        x++;
+                if (x > max)
+                {
+                    c = s[i];
+                    max = x;
+                }
+            }
+            Console.WriteLine("Znak {0} {1} puta", c, max);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema_ispit_pet3/Priprema_ispit_pet3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_ispit_pet3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, max = 1;
+            Console.WriteLine("Upišite prvi broj:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upišite drugi broj:");
+            y = Convert.ToInt32(Console.ReadLine());
+            z = x < y ? x : y;
+            for (int i = z; i >= 1; i--)
+                if (x % i == 0 && y % i == 0)
+                {
+                    max = i;
+                    break;
+                }
+            Console.WriteLine(max);
+
+            Console.ReadKey();
+        }
+    }
+}
